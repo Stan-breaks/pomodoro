17fca4349924ac660f35e302f236b2d9d1953edb
Stan-breaks <stanleymwendwa03@gmail.com>
2024-07-16 11:58:44 +0300

    notification function

--- utils/notify.go
+++ utils/notify.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Notify(notification models.Notification) {
-	cmd := exec.Command("notify-send", "Pomo", notification.Message)
+	cmd := exec.Command("notify-send", "Pomo", notification.Message, "-i", notification.IconPath)
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println(err)
