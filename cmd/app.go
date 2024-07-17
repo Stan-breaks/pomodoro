a97270e01c490dcb2066af0a29cc4c49f1e4bb15
Stan-breaks <stanleymwendwa03@gmail.com>
2024-07-17 20:23:36 +0300

    app

---
+++ cmd/app.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"pomo/cmd/routes"
+	"pomo/cmd/utils"
+
+	"golang.org/x/term"
+)
+
+func App() {
+	oldstate, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Println("Error setting raw mode:", err)
+		return
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldstate)
+	homePointer := 0
+	for {
+		utils.DisplayMenu(homePointer)
+		b := make([]byte, 1)
+		_, err = os.Stdin.Read(b)
+		if err != nil {
+			os.Stdout.Write([]byte("Error reading input \r\n"))
+			return
+		}
+		switch b[0] {
+		case 'w', 'W':
+			if homePointer > 0 {
+				homePointer--
+			}
+		case 's', 'S':
+			if homePointer < 5 {
+				homePointer++
+			}
+		case 'q', 'Q':
+			return
+		case 65:
+			if homePointer > 0 {
+				homePointer--
+			}
+		case 66:
+			if homePointer < 5 {
+				homePointer++
+			}
+    case 13, 10:
+    routes.HandleRoutes(homePointer)
+		}
+	}
+}
