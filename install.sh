5227d30b234fd716489b4a93ae2a7d00ea2c70c6
Stan-breaks <stanleymwendwa03@gmail.com>
2024-08-07 14:36:08 +0300

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/bash
 
+# Create directory and files
 mkdir -p "$HOME/.config/pomodoro"
 touch "$HOME/.config/pomodoro/logs.log"
 cat <<EOF >"$HOME/.config/pomodoro/custom.json"
 []
 EOF
+
+# Set correct permissions
+sudo chmod 755 "$HOME/.config/pomodoro"
+sudo chmod 644 "$HOME/.config/pomodoro/logs.log"
+sudo chmod 644 "$HOME/.config/pomodoro/custom.json"
+
+# Build the Go program
 go build -o pomo main.go
-sudo ln -sf "$(pwd)/pomo" /usr/local/bin/pomo
+
+# Install the program
+sudo install -o root -g root -m 755 pomo /usr/local/bin/pomo
+
+# Clean up
+rm pomo
